4089e731655cbaa2fcd1752d8161d771edf27b75
Ilija Jordanovski <jordanovski.sv73.2022@uns.ac.rs>
2025-06-06 14:20:36 +0200

    fix: university now knows how to convert string to enum

--- app/models/university.py
+++ app/models/university.py
@@ -1,4 +1,5 @@
-from sqlmodel import Field, SQLModel, String
+from pydantic import field_validator
+from sqlmodel import Field, SQLModel
 
 from models import register_model
 from enums.university import Country
@@ -12,6 +13,17 @@ class University(SQLModel, table = True):
             )
     name: str = Field(...)
     country: Country = Field(
-            sa_type=String,
             nullable=False
             )
+
+    @field_validator('country', mode='before')
+    @classmethod
+    def validate_country(cls, v):
+        if isinstance(v, str):
+            try:
+                return Country(v)
+            except ValueError:
+                raise ValueError(f"invalid country string, expected name of country but got {v}")
+        elif isinstance(v, Country):
+            return v
+        raise TypeError(f"expected country or string, got {type(v)}")
