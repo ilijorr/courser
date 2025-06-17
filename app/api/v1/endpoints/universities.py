1f76eb07497659d2019bdafe0998fc67158be18a
Ilija Jordanovski <jordanovski.sv73.2022@uns.ac.rs>
2025-06-17 17:48:04 +0200

    feat: added get for all with filtering

--- app/api/v1/endpoints/universities.py
+++ app/api/v1/endpoints/universities.py
@@ -1,7 +1,8 @@
-from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, Path, status
-from sqlmodel import Session
+from typing import Annotated, List
+from fastapi import APIRouter, Depends, HTTPException, Path, Query, status
+from sqlmodel import Session, select
 
+from enums.university import Country
 from rel_db import postgres
 from schemas.university import UniversityCreate, UniversityResponse
 from models.university import University as UniversityModel
@@ -40,6 +41,26 @@ def get_university(
                 detail="University not found")
     return UniversityResponse.model_validate(db_model)
 
+@router.get("/",
+            response_model=List[UniversityResponse])
+def get_universities_with_filter(
+        rel_db: Annotated[Session, Depends(postgres.get_db)],
+        country_name: Annotated[str | None, Query(max_length=50)] = None,
+       ):
+    query = select(UniversityModel)
+    if country_name is not None:
+        try:
+            country = Country(country_name.upper())
+            query = query.where(UniversityModel.country == country)
+        except ValueError:
+            raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="invalid country"
+                                )
+    universities = rel_db.exec(query).all()
+    return [UniversityModel.model_validate(university) 
+            for university in universities]
+
 @router.put("/{uni_id}",
             response_model=UniversityResponse,
             status_code=status.HTTP_200_OK)
@@ -61,3 +82,14 @@ def update_university(
     rel_db.commit()
     rel_db.refresh(db_model)
     return UniversityResponse.model_validate(db_model)
+
+@router.delete("/{uni_id}",
+               status_code=status.HTTP_204_NO_CONTENT)
+def delete_university(
+        uni_id: Annotated[int, Path(ge=1)],
+        rel_db: Annotated[Session, Depends(postgres.get_db)]
+        ):
+    query = select(UniversityModel).where(UniversityModel.id == uni_id)
+    results = rel_db.exec(query)
+    
+    
