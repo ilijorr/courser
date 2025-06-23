63ffac864aeaef0ffd760b7288cc7ceb9a4f016a
Ilija Jordanovski <jordanovski.sv73.2022@uns.ac.rs>
2025-06-23 13:09:29 +0200

    feat: first course endpoint, put still missing

--- app/api/v1/endpoints/courses.py
+++ app/api/v1/endpoints/courses.py
@@ -1,9 +1,11 @@
 from typing import Annotated, List
-
+from uuid import UUID
 from langchain_core.documents import Document
-from schemas.course import CourseCreate, CourseRelational
-from fastapi import APIRouter, Depends, status
-from sqlmodel import Session
+from sqlalchemy.exc import NoResultFound
+from enums.course import CourseCategory
+from schemas.course import Course, CourseCreate, CourseRelational
+from fastapi import APIRouter, Depends, HTTPException, Path, status, Query
+from sqlmodel import Session, select
 from langchain_core.vectorstores import VectorStore
 
 from rel_db import postgres
@@ -16,7 +18,7 @@ router = APIRouter(
         dependencies=[]
         )
 
-def to_document(course: CourseCreate) -> Document:
+def to_document(course: Course) -> Document:
     fields_to_exclude = set(CourseRelational.model_fields.keys())
     data = course.model_dump(exclude=fields_to_exclude)
     data["id"] = str(data["id"])
@@ -26,25 +28,148 @@ def to_document(course: CourseCreate) -> Document:
             )
 
 @router.post("/",
-             status_code=status.HTTP_201_CREATED)
+             status_code=status.HTTP_201_CREATED,
+             response_model=UUID)
 async def create_course(
         course: CourseCreate,
         rel_db: Annotated[Session, Depends(postgres.get_db)],
         vec_db: Annotated[VectorStore, Depends(pinecone.get_db)]
         ):
-    rel_db.add(CourseModel(**course.model_dump()))
-    rel_db.commit()
-    await vec_db.aadd_documents([to_document(course)])
+    course = Course(**course.model_dump())
+    try:
+        rel_db.add(CourseModel(**course.model_dump()))
+        rel_db.commit()
+    except Exception as e:
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"error while inserting into relational db"
+                )
+    try:
+        await vec_db.aadd_documents([to_document(course)])
+    except Exception as e:
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"error while upserting vector to db, {e}"
+                )
     return course.id
 
 @router.post("/batch",
-             status_code=status.HTTP_201_CREATED)
+             status_code=status.HTTP_201_CREATED,
+             response_model=List[UUID])
 async def create_course_batch(
-        courses: List[CourseCreate],
+        courses_without_id: List[CourseCreate],
         rel_db: Annotated[Session, Depends(postgres.get_db)],
         vec_db: Annotated[VectorStore, Depends(pinecone.get_db)]
         ):
-    rel_db.add_all([CourseModel(**course.model_dump()) for course in courses])
-    rel_db.commit()
-    await vec_db.aadd_documents([to_document(course) for course in courses])
+    try:
+        courses = [Course(**course.model_dump()) for course in courses_without_id]
+        rel_db.add_all([CourseModel(**course.model_dump()) for course in courses])
+        rel_db.commit()
+    except Exception as e:
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"error while inserting into relational db, {e}"
+                )
+    try:
+        await vec_db.aadd_documents([to_document(course) for course in courses])
+    except Exception as e:
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"error while upserting to vecdb, {e}"
+                )
     return [course.id for course in courses]
+
+@router.get("/",
+            status_code=status.HTTP_200_OK,
+            response_model=List[CourseModel])
+async def get_courses(
+        rel_db: Annotated[Session, Depends(postgres.get_db)],
+        uni_id: Annotated[int | None, Query(ge=1)] = None,
+        is_summer: Annotated[bool | None, Query()] = None,
+        category: Annotated[CourseCategory | None, Query()] = None,
+        ):
+    query = select(CourseModel)
+    if uni_id is not None:
+        query = query.where(CourseModel.uni_id == uni_id)
+    if is_summer is not None:
+        query = query.where(CourseModel.is_summer == is_summer)
+    if category is not None:
+        query = query.where(CourseModel.category == category)
+
+    try:
+        courses = rel_db.exec(query).all()
+    except Exception as e:
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"db error, {e}"
+                )
+
+    return courses
+
+@router.get("/{id}",
+            status_code=status.HTTP_200_OK,
+            response_model=CourseModel)
+async def get_course(
+        id: Annotated[UUID, Path()],
+        rel_db: Annotated[Session, Depends(postgres.get_db)],
+        ):
+    query = select(CourseModel).where(CourseModel.id == id)
+    try:
+        course_model = rel_db.exec(query).one()
+    except Exception as e:
+        return HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"id not found, {e}"
+                )
+    return course_model
+
+@router.get("/university/{uni_id}",
+            response_model=List[CourseModel])
+async def get_courses_for_university(
+        uni_id: Annotated[int, Path(..., ge=1)],
+        rel_db: Annotated[Session, Depends(postgres.get_db)]
+        ):
+    query = select(CourseModel).where(CourseModel.uni_id == uni_id)
+    try:
+        courses = rel_db.exec(query).all()
+    except Exception as e:
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"db error, {e}"
+                )
+    return courses
+
+@router.put("/{id}",
+            status_code=status.HTTP_501_NOT_IMPLEMENTED)
+async def put_course():
+    return None
+
+@router.delete("/{id}",
+               status_code=status.HTTP_204_NO_CONTENT)
+async def delete_course(
+        id: Annotated[UUID, Path(...)],
+        rel_db: Annotated[Session, Depends(postgres.get_db)],
+        vec_db: Annotated[VectorStore, Depends(pinecone.get_db)]
+        ):
+    query = select(CourseModel).where(CourseModel.id == id)
+    success = await vec_db.adelete([str(id)])
+    if success is False:
+        raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"course not found"
+                )
+    try:
+        course = rel_db.exec(query).one()
+        rel_db.delete(course)
+        rel_db.commit()
+    except NoResultFound as e:
+        raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"course not found, \n {e}"
+                )
+    except Exception as e:
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"relational db error, \n {e}"
+                )
+    return None
